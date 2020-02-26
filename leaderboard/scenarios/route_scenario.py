b32f4d3dcca7310575ae64a995c9b8bc989cf531
glopezdiest <glopez@cvc.uab.es>
2020-02-26 11:10:52 +0100

    Code formatting

--- leaderboard/scenarios/route_scenario.py
+++ leaderboard/scenarios/route_scenario.py
@@ -425,9 +425,9 @@ class RouteScenario(BasicScenario):
         elif town_name == 'Town06' or town_name == 'Town07':
             amount = 150
         elif town_name == 'Town08':
-            amount = 0 #180
+            amount = 180
         elif town_name == 'Town09':
-            amount = 0 #350
+            amount = 350
         else:
             amount = 1
 
