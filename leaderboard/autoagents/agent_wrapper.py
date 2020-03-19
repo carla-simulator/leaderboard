2d49793b3f754146bb8fb36354bb55825a0dc6ac
germanros1987 <38517452+germanros1987@users.noreply.github.com>
2020-03-19 07:00:22 -0700

    Germanros1987/feature speedometer (#24)
    * Feature: pseudo-sensor speedometer
    
    * Added a new speedometer pseudo-sensor
    * Changed default noise for IMU and GNSS
    
    * Fix: lack of id for speedometer sensor
    
    * added default reading_frequency for SpeedometerReader
    
    * fixed reading frequency in human_agent.py
    
    * refactorinfg of sensor_interface
    
    * Typo in OpenDRIVE
    
    * fixed problem with initial run in pseudo-sensor
    
    * Fixed problem with Speedometer init

--- leaderboard/autoagents/agent_wrapper.py
+++ leaderboard/autoagents/agent_wrapper.py
@@ -16,7 +16,7 @@ import os
 import carla
 from srunner.scenariomanager.carla_data_provider import CarlaDataProvider
 
-from leaderboard.envs.sensor_interface import CallBack, OpenDirveMapReader
+from leaderboard.envs.sensor_interface import CallBack, OpenDriveMapReader, SpeedometerReader
 from leaderboard.autoagents.autonomous_agent import Track
 
 MAX_ALLOWED_RADIUS_SENSOR = 3.0
@@ -27,7 +27,8 @@ SENSORS_LIMITS = {
     'sensor.other.radar': 2,
     'sensor.other.gnss': 1,
     'sensor.other.imu': 1,
-    'sensor.opendrive_map': 1
+    'sensor.opendrive_map': 1,
+    'sensor.speedometer': 1
 }
 
 
@@ -74,7 +75,9 @@ class AgentWrapper(object):
             # These are the pseudosensors (not spawned)
             if sensor_spec['type'].startswith('sensor.opendrive_map'):
                 # The HDMap pseudo sensor is created directly here
-                sensor = OpenDirveMapReader(vehicle, sensor_spec['reading_frequency'])
+                sensor = OpenDriveMapReader(vehicle, sensor_spec['reading_frequency'])
+            elif sensor_spec['type'].startswith('sensor.speedometer'):
+                sensor = SpeedometerReader(vehicle, sensor_spec['reading_frequency'])
             # These are the sensors spawned on the carla world
             else:
                 bp = bp_library.find(str(sensor_spec['type']))
@@ -117,11 +120,10 @@ class AgentWrapper(object):
                                                      roll=sensor_spec['roll'],
                                                      yaw=sensor_spec['yaw'])
 
-
                 elif sensor_spec['type'].startswith('sensor.other.gnss'):
-                    bp.set_attribute('noise_alt_stddev', str(1.5))
-                    bp.set_attribute('noise_lat_stddev', str(0.1))
-                    bp.set_attribute('noise_lon_stddev', str(0.1))
+                    bp.set_attribute('noise_alt_stddev', str(0.000001))
+                    bp.set_attribute('noise_lat_stddev', str(0.000001))
+                    bp.set_attribute('noise_lon_stddev', str(0.000001))
 
                     sensor_location = carla.Location(x=sensor_spec['x'],
                                                      y=sensor_spec['y'],
@@ -129,12 +131,12 @@ class AgentWrapper(object):
                     sensor_rotation = carla.Rotation()
 
                 elif sensor_spec['type'].startswith('sensor.other.imu'):
-                    bp.set_attribute('noise_accel_stddev_x', str(0.1))
-                    bp.set_attribute('noise_accel_stddev_y', str(0.1))
-                    bp.set_attribute('noise_accel_stddev_z', str(0.15))
-                    bp.set_attribute('noise_gyro_stddev_x', str(0.01))
-                    bp.set_attribute('noise_gyro_stddev_y', str(0.01))
-                    bp.set_attribute('noise_gyro_stddev_z', str(0.01))
+                    bp.set_attribute('noise_accel_stddev_x', str(0.001))
+                    bp.set_attribute('noise_accel_stddev_y', str(0.001))
+                    bp.set_attribute('noise_accel_stddev_z', str(0.015))
+                    bp.set_attribute('noise_gyro_stddev_x', str(0.001))
+                    bp.set_attribute('noise_gyro_stddev_y', str(0.001))
+                    bp.set_attribute('noise_gyro_stddev_z', str(0.001))
 
                     sensor_location = carla.Location(x=sensor_spec['x'],
                                                      y=sensor_spec['y'],
@@ -179,9 +181,9 @@ class AgentWrapper(object):
                         "Illegal sensor extrinsics used for Track [{}]!".format(self._agent.track))
 
             if sensor['type'] in sensor_count:
-               sensor_count[sensor['type']] += 1
+                sensor_count[sensor['type']] += 1
             else:
-               sensor_count[sensor['type']] = 0
+                sensor_count[sensor['type']] = 0
 
         for sensor_type, max_instances_allowed in SENSORS_LIMITS.items():
             if sensor_type in sensor_count and sensor_count[sensor_type] > max_instances_allowed:
@@ -190,7 +192,6 @@ class AgentWrapper(object):
                     "Maximum number allowed is {}, but {} were requested.".format(sensor_type,
                                                                                   max_instances_allowed,
                                                                                   sensor_count[sensor_type]))
-
 
     def cleanup(self):
         """
--- leaderboard/envs/sensor_interface.py
+++ leaderboard/envs/sensor_interface.py
@@ -8,6 +8,7 @@ from threading import Thread
 import carla
 from srunner.scenariomanager.carla_data_provider import CarlaDataProvider
 
+
 def threaded(fn):
     def wrapper(*args, **kwargs):
         thread = Thread(target=fn, args=args, kwargs=kwargs)
@@ -18,23 +19,24 @@ def threaded(fn):
     return wrapper
 
 
-class OpenDirveMapMeasurement(object):
+class GenericMeasurement(object):
     def __init__(self, data, frame):
         self.data = data
         self.frame = frame
 
 
-class  OpenDirveMapReader(object):
+class BaseReader(object):
     def __init__(self, vehicle, reading_frequency=1.0):
         self._vehicle = vehicle
         self._reading_frequency = reading_frequency
         self._callback = None
+        #  Counts the frames
         self._frame = 0
         self._run_ps = True
         self.run()
 
     def __call__(self):
-        return {'opendrive': CarlaDataProvider.get_map().to_opendrive()}
+        pass
 
     @threaded
     def run(self):
@@ -43,7 +45,7 @@ class  OpenDirveMapReader(object):
             if self._callback is not None:
                 capture = time.time()
                 if capture - latest_read > (1 / self._reading_frequency):
-                    self._callback(OpenDirveMapMeasurement(self.__call__(), self._frame))
+                    self._callback(GenericMeasurement(self.__call__(), self._frame))
                     self._frame += 1
                     latest_read = time.time()
                 else:
@@ -60,6 +62,49 @@ class  OpenDirveMapReader(object):
         self._run_ps = False
 
 
+class SpeedometerReader(BaseReader):
+    """
+    Sensor to measure the speed of the vehicle.
+    """
+    MAX_CONNECTION_ATTEMPTS = 10
+
+    def _get_forward_speed(self, transform=None, velocity=None):
+        """ Convert the vehicle transform directly to forward speed """
+        if not velocity:
+            velocity = self._vehicle.get_velocity()
+        if not transform:
+            transform = self._vehicle.get_transform()
+
+        vel_np = np.array([velocity.x, velocity.y, velocity.z])
+        pitch = np.deg2rad(transform.rotation.pitch)
+        yaw = np.deg2rad(transform.rotation.yaw)
+        orientation = np.array([np.cos(pitch) * np.cos(yaw), np.cos(pitch) * np.sin(yaw), np.sin(pitch)])
+        speed = np.dot(vel_np, orientation)
+        return speed
+
+    def __call__(self):
+        """ We convert the vehicle physics information into a convenient dictionary """
+
+        # protect this access against timeout
+        attempts = 0
+        while attempts < self.MAX_CONNECTION_ATTEMPTS:
+            try:
+                velocity = self._vehicle.get_velocity()
+                transform = self._vehicle.get_transform()
+                break
+            except Exception:
+                attempts += 1
+                time.sleep(0.2)
+                continue
+
+        return {'speed': self._get_forward_speed(transform=transform, velocity=velocity)}
+
+
+class OpenDriveMapReader(BaseReader):
+    def __call__(self):
+        return {'opendrive': CarlaDataProvider.get_map().to_opendrive()}
+
+
 class CallBack(object):
     def __init__(self, tag, sensor, data_provider):
         self._tag = tag
@@ -68,7 +113,6 @@ class CallBack(object):
         self._data_provider.register_sensor(tag, sensor)
 
     def __call__(self, data):
-
         if isinstance(data, carla.libcarla.Image):
             self._parse_image_cb(data, self._tag)
         elif isinstance(data, carla.libcarla.LidarMeasurement):
@@ -79,7 +123,7 @@ class CallBack(object):
             self._parse_gnss_cb(data, self._tag)
         elif isinstance(data, carla.libcarla.IMUMeasurement):
             self._parse_imu_cb(data, self._tag)
-        elif isinstance(data, OpenDirveMapMeasurement):
+        elif isinstance(data, GenericMeasurement):
             self._parse_pseudosensor(data, self._tag)
         else:
             logging.error('No callback method for this sensor.')
@@ -124,6 +168,7 @@ class CallBack(object):
 
     def _parse_pseudosensor(self, package, tag):
         self._data_provider.update_sensor(tag, package.data, package.frame)
+
 
 class SensorInterface(object):
     def __init__(self):
