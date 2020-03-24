ef5bbab3c588b8adad3942db0a08835bb74918c4
germanros1987 <38517452+germanros1987@users.noreply.github.com>
2020-03-23 22:18:49 -0700

    Germanros1987/gnss fix (#31)
    * attempt to fix gnns discrepancies
    
    * Fixed small bug in gnss (negated -y)

--- leaderboard/autoagents/agent_wrapper.py
+++ leaderboard/autoagents/agent_wrapper.py
@@ -121,9 +121,12 @@ class AgentWrapper(object):
                                                      yaw=sensor_spec['yaw'])
 
                 elif sensor_spec['type'].startswith('sensor.other.gnss'):
-                    bp.set_attribute('noise_alt_stddev', str(0.000001))
-                    bp.set_attribute('noise_lat_stddev', str(0.000001))
-                    bp.set_attribute('noise_lon_stddev', str(0.000001))
+                    bp.set_attribute('noise_alt_stddev', str(0.000005))
+                    bp.set_attribute('noise_lat_stddev', str(0.000005))
+                    bp.set_attribute('noise_lon_stddev', str(0.000005))
+                    bp.set_attribute('noise_alt_bias', str(0.0))
+                    bp.set_attribute('noise_lat_bias', str(0.0))
+                    bp.set_attribute('noise_lon_bias', str(0.0))
 
                     sensor_location = carla.Location(x=sensor_spec['x'],
                                                      y=sensor_spec['y'],
