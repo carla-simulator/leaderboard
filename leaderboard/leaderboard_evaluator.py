06ed6a6832d9ba094fa734a86a670de7d887fee8
German Ros <germanros1987@gmail.com>
2020-04-02 17:20:09 -0700

    Remove torch in leaderboard

--- leaderboard/leaderboard_evaluator.py
+++ leaderboard/leaderboard_evaluator.py
@@ -12,7 +12,6 @@ Provisional code to evaluate Autonomous Agents for the CARLA Autonomous Driving 
 """
 from __future__ import print_function
 
-import torch
 import traceback
 import argparse
 from argparse import RawTextHelpFormatter
