783b3365141d9c5358dead8d78c42392df15fbcc
glopezdiest <glopez@cvc.uab.es>
2020-09-16 12:34:46 +0200

    WIP: Log and Playback to human agent

--- leaderboard/autoagents/autonomous_agent.py
+++ leaderboard/autoagents/autonomous_agent.py
@@ -110,7 +110,7 @@ class AutonomousAgent(object):
         wallclock = GameTime.get_wallclocktime()
         wallclock_diff = (wallclock - self.wallclock_t0).total_seconds()
 
-        print('======[Agent] Wallclock_time = {} / {} / Sim_time = {} / {}x'.format(wallclock, wallclock_diff, timestamp, timestamp/(wallclock_diff+0.001)))
+        # print('======[Agent] Wallclock_time = {} / {} / Sim_time = {} / {}x'.format(wallclock, wallclock_diff, timestamp, timestamp/(wallclock_diff+0.001)))
 
         control = self.run_step(input_data, timestamp)
         control.manual_gear_shift = False
