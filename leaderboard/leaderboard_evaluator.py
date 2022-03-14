40051e9737efd086424f1c5a9ceb166a3814a225
glopezdiest <58212725+glopezdiest@users.noreply.github.com>
2022-03-14 11:52:55 +0100

    Scenarios are now route specific

--- leaderboard/leaderboard_evaluator.py
+++ leaderboard/leaderboard_evaluator.py
@@ -382,7 +382,7 @@ class LeaderboardEvaluator(object):
         """
         Run the challenge mode
         """
-        route_indexer = RouteIndexer(args.routes, args.scenarios, args.repetitions, args.route_id)
+        route_indexer = RouteIndexer(args.routes, args.repetitions, args.route_id)
 
         if args.resume:
             route_indexer.resume(args.checkpoint)
@@ -413,39 +413,38 @@ def main():
     parser = argparse.ArgumentParser(description=description, formatter_class=RawTextHelpFormatter)
     parser.add_argument('--host', default='localhost',
                         help='IP of the host server (default: localhost)')
-    parser.add_argument('--port', default=2000, type=int, help='TCP port to listen to (default: 2000)')
+    parser.add_argument('--port', default=2000, type=int,
+                        help='TCP port to listen to (default: 2000)')
     parser.add_argument('--traffic-manager-port', default=8000, type=int,
                         help='Port to use for the TrafficManager (default: 8000)')
     parser.add_argument('--traffic-manager-seed', default=0, type=int,
                         help='Seed used by the TrafficManager (default: 0)')
-    parser.add_argument('--debug', type=int, help='Run with debug output', default=0)
+    parser.add_argument('--debug', type=int,
+                        help='Run with debug output', default=0)
     parser.add_argument('--record', type=str, default='',
                         help='Use CARLA recording feature to create a recording of the scenario')
     parser.add_argument('--timeout', default=60.0, type=float,
                         help='Set the CARLA client timeout value in seconds')
 
     # simulation setup
-    parser.add_argument('--routes',
-                        help='Name of the route to be executed. Point to the route_xml_file to be executed.',
-                        required=True)
+    parser.add_argument('--routes', required=True,
+                        help='Name of the route to be executed. Point to the route_xml_file to be executed.')
     parser.add_argument('--route-id', default='', type=str,
                         help='Execute a specific route')
-    parser.add_argument('--scenarios',
-                        help='Name of the scenario annotation file to be mixed with the route.',
-                        required=True)
-    parser.add_argument('--repetitions',
-                        type=int,
-                        default=1,
+    parser.add_argument('--repetitions', type=int, default=1,
                         help='Number of repetitions per route.')
 
     # agent-related options
-    parser.add_argument("-a", "--agent", type=str, help="Path to Agent's py file to evaluate", required=True)
-    parser.add_argument("--agent-config", type=str, help="Path to Agent's configuration file", default="")
+    parser.add_argument("-a", "--agent", type=str,
+                        help="Path to Agent's py file to evaluate", required=True)
+    parser.add_argument("--agent-config", type=str,
+                        help="Path to Agent's configuration file", default="")
 
-    parser.add_argument("--track", type=str, default='SENSORS', help="Participation track: SENSORS, MAP")
-    parser.add_argument('--resume', type=bool, default=False, help='Resume execution from last checkpoint?')
-    parser.add_argument("--checkpoint", type=str,
-                        default='./simulation_results.json',
+    parser.add_argument("--track", type=str, default='SENSORS',
+                        help="Participation track: SENSORS, MAP")
+    parser.add_argument('--resume', type=bool, default=False,
+                        help='Resume execution from last checkpoint?')
+    parser.add_argument("--checkpoint", type=str, default='./simulation_results.json',
                         help="Path to checkpoint used for saving statistics and resuming")
 
     arguments = parser.parse_args()
