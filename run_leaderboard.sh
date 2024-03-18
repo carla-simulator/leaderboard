eb818a12b4f78f153002f55343442cdb6e63e8f5
glopezdiest <glopez@cvc.uab.cat>
2024-03-18 10:37:20 +0100

    Modified initial scripts

--- run_leaderboard.sh
+++ run_leaderboard.sh
@@ -1,3 +1,17 @@
+ #!/bin/bash
+
+export TEAM_AGENT=$LEADERBOARD_ROOT/leaderboard/autoagents/human_agent.py
+
+export ROUTES=$LEADERBOARD_ROOT/data/routes_training.xml
+export ROUTES_SUBSET=0
+export REPETITIONS=1
+
+export DEBUG_CHALLENGE=0
+export CHALLENGE_TRACK_CODENAME=SENSORS
+export CHECKPOINT_ENDPOINT="results.json"
+export RECORD_PATH=
+export RESUME=
+
 #!/bin/bash
 
 python3 ${LEADERBOARD_ROOT}/leaderboard/leaderboard_evaluator.py \
